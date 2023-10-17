9a8a8b5524e95d5273da8ec97cbd04cdba2c3b0b
Jeremias Ivan <jeremias.ivan98@gmail.com>
2023-10-17 17:11:18 +1100

    Add function and .Rmd to Mash Screen short reads

--- codes/2_mash_sketch/functions.R
+++ codes/2_mash_sketch/functions.R
@@ -1,10 +1,16 @@
 # functions for codes/2_mash_sketch
 
-# function: create Mash Sketch for all reference alignments
+# function: create Mash sketch for all reference alignments
 f_mash_sketch <- function(exe_mash, str_refseq, thread, prefix) {
     cmd <- paste(exe_mash, "sketch",
                 "-o", prefix,
                 "-p", thread,
                 str_refseq)
     system(cmd)
+}
+
+# function: run Mash screen on short reads
+f_mash_screen <- function(exe_mash, file_msh, file_input, file_output) {
+    cmd_mash <- paste(exe_mash, "screen -w", file_msh, file_input, ">", file_output)
+    system(cmd_mash)
 }
