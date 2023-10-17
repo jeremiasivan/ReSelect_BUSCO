74acd90561150cde098e4c8262fe2e63b6835d1a
Jeremias Ivan <jeremias.ivan98@gmail.com>
2023-10-17 14:18:56 +1100

    Add function and .Rmd to download reference sequence

--- codes/1_data_download/functions.R
+++ codes/1_data_download/functions.R
@@ -1,1 +1,13 @@
 # functions for codes/1_data_download
+
+# function: download references FASTA sequence using NCBI Datasets
+f_refseq_download <- function(dir_datasets, accession, file_output) {
+    cmd <- paste(dir_datasets, "download genome",
+                "accession", accession,
+                "--filename", file_output,
+                "--include genome --no-progressbar")
+
+    system(cmd)
+    system(paste("unzip", file_output))
+}
+
