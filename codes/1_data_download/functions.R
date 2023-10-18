29834aacd061bd4282edd9f1716633c4042cefaf
Jeremias Ivan <jeremias.ivan98@gmail.com>
2023-10-18 13:50:32 +1100

    Bugfix on unzipping refseq

--- codes/1_data_download/functions.R
+++ codes/1_data_download/functions.R
@@ -1,14 +1,14 @@
 # functions for codes/1_data_download
 
 # function: download references FASTA sequence using NCBI Datasets
-f_refseq_download <- function(dir_datasets, accession, file_output) {
+f_refseq_download <- function(dir_datasets, accession, file_output, dir_refseq) {
     cmd <- paste(dir_datasets, "download genome",
                 "accession", accession,
                 "--filename", file_output,
                 "--include genome --no-progressbar")
 
     system(cmd)
-    system(paste("unzip", file_output))
+    system(paste("unzip", file_output, dir_refseq))
 }
 
 # function: download short reads FASTQ sequence using SRA Toolkit
