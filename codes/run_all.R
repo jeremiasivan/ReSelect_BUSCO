02028723b1ecf0ae3de122badbe8afbeecd59f04
Jeremias Ivan <jeremias.ivan98@gmail.com>
2023-10-18 12:50:58 +1100

    Add run_all.R for the first two steps

---
+++ codes/run_all.R
@@ -0,0 +1,37 @@
+#################################
+codedir <- "~/BusIER/codes"
+prefix <- ""
+outdir <- "~/busco"
+thread <- 5
+redo <- TRUE
+
+# data download
+file_refseq <- ""
+file_shortreads <- ""
+
+exe_datasets <- ""
+dir_sratoolkit <- ""
+
+# mash sketch
+exe_mash <- ""
+exe_esearch <- ""
+exe_efetch <- ""
+exe_xtract <- ""
+#################################
+
+# data download
+rmarkdown::render(input=paste0(codedir,"/1_data_download/1_main.Rmd"),
+                  output_file=paste0(outdir, "/", prefix, ".download.html"),
+                  params=list(prefix=prefix, codedir=codedir, outdir=outdir, thread=thread, redo=redo,
+                              file_refseq=file_refseq, file_shortreads=file_shortreads,
+                              exe_datasets=exe_datasets, dir_sratoolkit=dir_sratoolkit),
+                  quiet=TRUE)
+
+# mash sketch
+rmarkdown::render(input=paste0(codedir,"/2_mash_sketch/1_main.Rmd"),
+                  output_file=paste0(outdir, "/", prefix, ".mash.html"),
+                  params=list(prefix=prefix, codedir=codedir, outdir=outdir, thread=thread, redo=redo,
+                              exe_mash=exe_mash, exe_esearch=exe_esearch, exe_efetch=exe_efetch, exe_xtract=exe_xtract),
+                  quiet=TRUE)
+
+#################################
