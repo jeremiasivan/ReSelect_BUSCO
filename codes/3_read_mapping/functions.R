ad4afdc7a1c0897c68766780cdc79c4bfcb053d6
Jeremias Ivan <jeremias.ivan98@gmail.com>
2023-10-24 15:27:43 +1100

    Add preliminary analysis for SAM to BAM and FASTA

--- codes/3_read_mapping/functions.R
+++ codes/3_read_mapping/functions.R
@@ -17,3 +17,47 @@ f_read_map <- function(refseq, fastq, thread, exe_bwamem2, file_sam) {
     }
     system(cmd_readmap)
 }
+
+# function: convert SAM to BAM and FASTA
+f_convert_SAM <- function(prefix, dir_sam, dir_bam, dir_fasta, thread, exe_samtools) {
+    samtools_thread <- paste0("@", thread)
+
+    fn_sam <- paste0(dir_sam, "/", prefix, ".sam")
+    fn_bam_fixmate <- paste0(dir_bam, "/", prefix, ".fixmate.bam")
+    fn_bam_sort <- paste0(dir_bam, "/", prefix, ".sort.bam")
+    fn_bam_markdup <- paste0(dir_bam, "/", prefix, ".markdup.bam")
+    fn_bam <- paste0(dir_bam, "/", prefix, ".bam")
+    fn_fasta <- paste0(dir_fasta, "/", prefix, ".fa")
+
+    # run samtools fixmate
+    cmd_fixmate <- paste(exe_samtools, "fixmate",
+                         samtools_thread,
+                         "-O bam",
+                         "-m", fn_sam, fn_bam_fixmate)
+    system(cmd_fixmate)
+
+    # run samtools sort
+    cmd_sort <- paste(exe_samtools, "sort",
+                      samtools_thread,
+                      "-o", fn_bam_sort, fn_bam_fixmate)
+    system(cmd_sort)                  
+    
+    # run samtools markdup
+    cmd_markdup <- paste(exe_samtools, "markdup",
+                         samtools_thread,
+                         "-O bam",
+                         fn_bam_sort, fn_bam_markdup) 
+    system(cmd_markdup)
+
+    # run samtools view to BAM
+    cmd_view <- paste(exe_samtools, "view",
+                      samtools_thread,
+                      fn_bam_markdup, fn_bam)
+    system(cmd_view)
+
+    # run samtools fasta
+    cmd_fasta <- paste(exe_samtools, "fasta",
+                       samtools_thread,
+                       fn_bam, ">", fn_fasta)                                          
+    system(cmd_fasta)
+}
