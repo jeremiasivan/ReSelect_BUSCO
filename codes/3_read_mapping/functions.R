5b755f5034f7cef4a5555a21bbcdcda715c1d3c7
Jeremias Ivan <jeremias.ivan98@gmail.com>
2023-11-15 08:41:56 +1100

    Update flag for samtools thread

--- codes/3_read_mapping/functions.R
+++ codes/3_read_mapping/functions.R
@@ -20,8 +20,6 @@ f_read_map <- function(refseq, fastq, thread, exe_bwamem2, file_sam) {
 
 # function: convert SAM to BAM and FASTA
 f_convert_SAM <- function(prefix, dir_sam, dir_bam, dir_fasta, thread, exe_samtools) {
-    samtools_thread <- paste0("@", thread)
-
     fn_sam <- paste0(dir_sam, "/", prefix, ".sam")
     fn_bam_fixmate <- paste0(dir_bam, "/", prefix, ".fixmate.bam")
     fn_bam_sort <- paste0(dir_bam, "/", prefix, ".sort.bam")
@@ -31,33 +29,33 @@ f_convert_SAM <- function(prefix, dir_sam, dir_bam, dir_fasta, thread, exe_samto
 
     # run samtools fixmate
     cmd_fixmate <- paste(exe_samtools, "fixmate",
-                         samtools_thread,
+                         "--threads", thread,
                          "-O bam",
                          "-m", fn_sam, fn_bam_fixmate)
     system(cmd_fixmate)
 
     # run samtools sort
     cmd_sort <- paste(exe_samtools, "sort",
-                      samtools_thread,
+                      "--threads", thread,
                       "-o", fn_bam_sort, fn_bam_fixmate)
     system(cmd_sort)                  
     
     # run samtools markdup
     cmd_markdup <- paste(exe_samtools, "markdup",
-                         samtools_thread,
+                         "--threads", thread,
                          "-O bam",
                          fn_bam_sort, fn_bam_markdup) 
     system(cmd_markdup)
 
     # run samtools view to BAM
     cmd_view <- paste(exe_samtools, "view",
-                      samtools_thread,
+                      "--threads", thread,
                       fn_bam_markdup, fn_bam)
     system(cmd_view)
 
     # run samtools fasta
     cmd_fasta <- paste(exe_samtools, "fasta",
-                       samtools_thread,
+                       "--threads", thread,
                        fn_bam, ">", fn_fasta)                                          
     system(cmd_fasta)
 }
