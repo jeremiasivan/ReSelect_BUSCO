0f2831b1dc45cee09b89b5116d48fa70530e0eb4
Jeremias Ivan <jeremias.ivan98@gmail.com>
2023-11-16 16:17:21 +1100

    Update SAM to FASTA conversion

--- codes/3_read_mapping/functions.R
+++ codes/3_read_mapping/functions.R
@@ -18,14 +18,37 @@ f_read_map <- function(refseq, fastq, thread, exe_bwamem2, file_sam) {
     system(cmd_readmap)
 }
 
+# function: combine multiple FASTA headers into concatenated sequence
+f_combine_fasta_headers <- function(header, file_fasta, file_output) {
+    # initialize variable
+    combined_fasta <- paste0('>', header, "\n")
+
+    # initiate connection
+    con <- file(file_fasta, open = "r")
+
+    # iterate through lines
+    while (length(line <- readLines(con, n=1)) > 0) {
+        if (!grepl("^>", line)) {
+            combined_fasta <- paste0(combined_fasta, line, "\n")
+        }
+    }
+
+    # close connection
+    close(con)
+
+    # write output file
+    writeLines(combined_fasta, file_output)
+}
+
 # function: convert SAM to BAM and FASTA
-f_convert_SAM <- function(prefix, dir_sam, dir_bam, dir_fasta, thread, exe_samtools) {
-    fn_sam <- paste0(dir_sam, "/", prefix, ".sam")
-    fn_bam_fixmate <- paste0(dir_bam, "/", prefix, ".fixmate.bam")
-    fn_bam_sort <- paste0(dir_bam, "/", prefix, ".sort.bam")
-    fn_bam_markdup <- paste0(dir_bam, "/", prefix, ".markdup.bam")
-    fn_bam <- paste0(dir_bam, "/", prefix, ".bam")
-    fn_fasta <- paste0(dir_fasta, "/", prefix, ".fa")
+f_convert_SAM <- function(prefix, dir_output, thread, exe_samtools) {
+    fn_sam <- paste0(dir_output, "/", prefix, ".sam")
+    fn_bam_fixmate <- paste0(dir_output, "/", prefix, ".fixmate.bam")
+    fn_bam_sort <- paste0(dir_output, "/", prefix, ".sort.bam")
+    fn_bam_markdup <- paste0(dir_output, "/", prefix, ".markdup.bam")
+    fn_bam <- paste0(dir_output, "/", prefix, ".bam")
+    fn_fasta_one <- paste0(dir_output, "/", prefix, ".1.fa")
+    fn_fasta_two <- paste0(dir_output, "/", prefix, ".2.fa")
 
     # run samtools fixmate
     cmd_fixmate <- paste(exe_samtools, "fixmate",
@@ -58,14 +81,21 @@ f_convert_SAM <- function(prefix, dir_sam, dir_bam, dir_fasta, thread, exe_samto
     # run samtools fasta
     cmd_fasta <- paste(exe_samtools, "fasta",
                        "--threads", thread,
-                       fn_bam, ">", fn_fasta)                                          
+                       "-1", fn_fasta_one, "-2", fn_fasta_two,
+                       fn_bam)                                          
     system(cmd_fasta)
+
+    # run FASTA header concatenation
+    fn_fasta_concat_one <- paste0(dir_output, "/", prefix, ".1.fa")
+    fn_fasta_concat_two <- paste0(dir_output, "/", prefix, ".2.fa")
+    f_combine_fasta_headers(prefix, fn_fasta_one, fn_fasta_concat_one)
+    f_combine_fasta_headers(prefix, fn_fasta_two, fn_fasta_concat_two)
 }
 
-f_index_reference <- function(refseq, exe_samtools) {
-    cmd_index <- paste(exe_samtools, "faidx", refseq)
-    system(cmd_index)
-}
+#f_index_reference <- function(refseq, exe_samtools) {
+#    cmd_index <- paste(exe_samtools, "faidx", refseq)
+#    system(cmd_index)
+#}
 
 #f_variant_call <- function(prefix, refseq, bam, chromosome, dir_vcf, exe_bcftools){
 #    fn_bcf <- paste0(dir_vcf, "/", prefix, ".genotype_likelihood.bcf")
