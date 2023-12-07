c8bc081359bd34cbc3bd4d8f21fc5b37727d3d6e
Jeremias Ivan <jeremias.ivan98@gmail.com>
2023-12-07 18:58:47 +1100

    Add initial BUSCO check

---
+++ codes/2_busco_check/functions.R
@@ -0,0 +1,109 @@
+# functions for codes/2_busco_check
+
+# function: read map fastq to reference sequence using BWA-MEM2
+f_read_map <- function(refseq, fastq, thread, exe_bwamem2, file_sam) {
+    # index reference file
+    cmd_index <- paste(exe_bwamem2, "index", refseq)
+    system(cmd_index)
+
+    # read-map
+    cmd_readmap <- paste(exe_bwamem2, "mem -t", thread, refseq)
+
+    len_fastq <- length(fastq)
+    if (len_fastq == 1) {
+        cmd_readmap <- paste(cmd_readmap, fastq[1], ">", file_sam)
+    } else if (len_fastq == 2) {
+        cmd_readmap <- paste(cmd_readmap, fastq[1], fastq[2], ">", file_sam)
+    }
+    system(cmd_readmap)
+}
+
+# function: convert SAM to BAM and FASTA
+f_convert_sam2bam <- function(prefix, dir_output, thread, exe_samtools) {
+    fn_sam <- paste0(dir_output, "/", prefix, ".sam")
+    fn_bam_fixmate <- paste0(dir_output, "/", prefix, ".fixmate.bam")
+    fn_bam_sort <- paste0(dir_output, "/", prefix, ".sort.bam")
+    fn_bam_markdup <- paste0(dir_output, "/", prefix, ".markdup.bam")
+    fn_bam <- paste0(dir_output, "/", prefix, ".bam")
+
+    # run samtools fixmate
+    cmd_fixmate <- paste(exe_samtools, "fixmate",
+                         "--threads", thread,
+                         "-O bam,level=1",
+                         "-m", fn_sam, fn_bam_fixmate)
+    system(cmd_fixmate)
+
+    # run samtools sort
+    cmd_sort <- paste(exe_samtools, "sort",
+                      "--threads", thread,
+                      "-l 1",
+                      "-o", fn_bam_sort, fn_bam_fixmate)
+    system(cmd_sort)                  
+    
+    # run samtools markdup
+    cmd_markdup <- paste(exe_samtools, "markdup",
+                         "--threads", thread,
+                         "-O bam,level=1",
+                         fn_bam_sort, fn_bam_markdup) 
+    system(cmd_markdup)
+
+    # run samtools view to BAM
+    cmd_view <- paste(exe_samtools, "view",
+                      "--threads", thread,
+                      fn_bam_markdup, 
+                      "-o", fn_bam)
+    system(cmd_view)
+}
+
+# function: run BUSCO pipeline
+f_run_busco <- function(fn_fasta, lineage, prefix, dir_output, mode, thread, exe_busco) {
+    cmd_busco <- paste(exe_busco,
+                       "-i", fn_fasta,
+                       "-l", lineage,
+                       "-m", mode,
+                       "-o", prefix,
+                       "--out_path", dir_output,
+                       "-c", thread)
+    system(cmd_busco)
+}
+
+# functions: extract BUSCO region
+f_extract_busco_from_BAM <- function(fn_bam, fn_out_bam, coordinates, fn_out_1, fn_out_2, exe_samtools) {
+    # run samtools view
+    cmd_view <- paste(exe_samtools, "view",
+                      "-b", fn_bam,
+                      coordinates, ">", fn_out_bam)
+    system(cmd_view)
+
+    # run samtools fasta
+    cmd_fasta <- paste(exe_samtools, "fasta",
+                       "-1", fn_out_1, "-2", fn_out_2,
+                       fn_out_bam)                                          
+    system(cmd_fasta)
+}
+
+# functions: combine individual FASTA as MSA
+f_fasta2msa <- function(fn_input, header, fn_out) {
+    # open the FASTA file
+    con <- file(fn_input, "r")
+
+    # iterate over lines
+    while (length(line <- readLines(con, n = 1)) > 0) {
+        if (grepl("^>+", line)) {
+            write.table(paste0(">", header), file=fn_out, quote=F, row.names=F, col.names=F, append=T)
+        } else {
+            write.table(line, file=fn_out, quote=F, row.names=F, col.names=F, append=T)
+        }
+    }
+    
+    # close the file connection
+    close(con)
+}
+
+# functions: run IQ-Tree 2
+f_iqtree2 <- function(fn_input, exe_iqtree2) {
+    cmd_iqtree2 <- paste(exe_iqtree2,
+                         "-s", fn_input,
+                         "-T 1 --quiet -redo")
+    system(cmd_iqtree2)
+}
--- codes/run_all.R
+++ codes/run_all.R
@@ -9,16 +9,19 @@ redo <- TRUE
 file_refseq <- ""
 file_shortreads <- ""
 
-exe_datasets <- ""
-dir_sratoolkit <- ""
+exe_datasets <- "datasets"
+dir_sratoolkit <- "sratoolkit/bin/"
 
-# mash sketch
-exe_mash <- ""
-exe_efetch <- ""
+# BUSCO check
+exe_bwamem2 <- "bwa-mem2"
+exe_samtools <- "samtools"
+exe_busco <- "busco"
+exe_iqtree2 <- "iqtree2"
 
-# read mapping
-exe_bwamem2 <- ""
-exe_samtools <- ""
+n_short_reads <- 3
+
+busco_lineage <- "eudicots_odb10"
+busco_mode <- "genome"
 #################################
 
 # set up outdir
@@ -35,18 +38,12 @@ rmarkdown::render(input=paste0(codedir,"/1_data_download/1_main.Rmd"),
                               exe_datasets=exe_datasets, dir_sratoolkit=dir_sratoolkit),
                   quiet=TRUE)
 
-# mash sketch
-rmarkdown::render(input=paste0(codedir,"/2_mash_sketch/1_main.Rmd"),
-                  output_file=paste0(outdir_prefix, prefix, ".mash.html"),
+# BUSCO check
+rmarkdown::render(input=paste0(codedir,"/2_busco_check/1_main.Rmd"),
+                  output_file=paste0(outdir_prefix, prefix, ".check.html"),
                   params=list(prefix=prefix, codedir=codedir, outdir=outdir, thread=thread, redo=redo,
-                              exe_mash=exe_mash, exe_efetch=exe_efetch),
-                  quiet=TRUE)
-
-# read mapping
-rmarkdown::render(input=paste0(codedir,"/3_read_mapping/1_main.Rmd"),
-                  output_file=paste0(outdir_prefix, prefix, ".readmap.html"),
-                  params=list(prefix=prefix, codedir=codedir, outdir=outdir, thread=thread, redo=redo,
-                              exe_bwamem2=exe_bwamem2, exe_samtools=exe_samtools),
+                              exe_bwamem2=exe_bwamem2, exe_samtools=exe_samtools, exe_busco=exe_busco, exe_iqtree2=exe_iqtree2,
+                              n_short_reads=n_short_reads, busco_lineage=busco_lineage, busco_mode=busco_mode),
                   quiet=TRUE)
 
 #################################
