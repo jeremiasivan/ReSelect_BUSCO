b4e5d67506d7c61ccb32a54442d18c5a1c7ec6fe
Jeremias Ivan <jeremias.ivan98@gmail.com>
2024-01-23 14:47:50 +1100

    Update BUSCO sequences retrieval

--- codes/2_busco_check/functions.R
+++ codes/2_busco_check/functions.R
@@ -90,40 +90,22 @@ f_run_busco <- function(fn_fasta, lineage, prefix, dir_output, mode, thread, exe
 }
 
 # functions: extract BUSCO region
-f_extract_busco <- function(fn_vcf, refseq, busco_header, fn_out, exe_samtools, exe_bcftools) {
-    # set up variable
-    is_reverse <- FALSE
-
+f_extract_busco <- function(busco_header, fn_gff, all_seqs, fn_out) {
     # remove > sign
     no_header <- unlist(strsplit(busco_header, split=">"))[2]
 
     # extract start and stop coordinates
     ls_coordinates <- unlist(strsplit(no_header, split=":"))
-    start_coordinate <- as.numeric(unlist(strsplit(ls_coordinates[2], split="-"))[1]) + 1
-    stop_coordinate <- as.numeric(unlist(strsplit(ls_coordinates[2], split="-"))[2]) + 1
 
-    # update the coordinates
-    coordinates <- paste0(ls_coordinates[1], ":", start_coordinate, "-", stop_coordinate)
-    if (start_coordinate > stop_coordinate) {
-        coordinates <- paste0(ls_coordinates[1], ":", stop_coordinate, "-", start_coordinate)
-        is_reverse <- TRUE
-    }
+    # read GFF table
+    df_gff <- data.table::fread(fn_gff)
+    metadata <- df_gff$V9[df_gff$V3=="gene" & df_gff$V1==ls_coordinates[1]]
+    targetid <- unlist(strsplit(metadata, split=";"))[1]
+    targetid <- unlist(strsplit(targetid, split="="))[2]
 
-    # extract consensus BUSCO sequence
-    cmd_consensus <- paste(exe_samtools, "faidx", refseq, coordinates, "|",
-                           exe_bcftools, "consensus", fn_vcf, "-o", fn_out)
-    if (is_reverse) {
-        cmd_consensus <- paste0(cmd_consensus, "_temp")
-    }                                   
-    system(cmd_consensus)
-
-    # reverse complement the sequence
-    if (is_reverse) {
-        fasta_sequence <- Biostrings::readDNAStringSet(paste0(fn_out, "_temp"))
-        rc_fasta_sequence <- Biostrings::reverseComplement(fasta_sequence)
-        Biostrings::writeXStringSet(rc_fasta_sequence, filepath=fn_out)
-        unlink(paste0(fn_out, "_temp"))
-    }
+    # extract the FASTA sequence
+    busco_seq <- all_seqs[grepl(targetid, names(all_seqs))]
+    Biostrings::writeXStringSet(busco_seq, filepath=fn_out)
 }
 
 # functions: combine individual FASTA as MSA
