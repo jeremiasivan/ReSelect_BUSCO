a11ddaa780b79aaea349056e7b32ef899515045f
Jeremias Ivan <jeremias.ivan98@gmail.com>
2025-07-18 12:25:08 +1000

    Bugfix

--- codes/1_data_preparation/functions.R
+++ codes/1_data_preparation/functions.R
@@ -15,17 +15,17 @@ f_refseq_download <- function(dir_datasets, accession, file_output) {
 }
 
 # function: download short reads FASTQ sequence using SRA Toolkit
-f_shortreads_download <- function(dir_sratoolkit, accession, dir_output) {
+f_shortreads_download <- function(dir_sratoolkit, accession, read, dir_output) {
     # prefetch from NCBI
     exe_prefetch <- paste0(dir_sratoolkit,"/prefetch")
     cmd_download <- paste(exe_prefetch, "--output-directory", dir_output, accession)
     system(cmd_download)
 
     # output SRA file
-    file_sra <- paste0(dir_output,"/",accession,"/",accession,".sra")
+    file_sra <- paste0(dir_output,"/",read,"/",accession,".sra")
     
     # create fastq folder
-    dir_fastq <- paste0(dir_output,"/",accession,"/fastq/")
+    dir_fastq <- paste0(dir_output,"/",read,"/fastq/")
     if (!dir.exists(dir_fastq)) {
         dir.create(dir_fastq, recursive=T)
     }
