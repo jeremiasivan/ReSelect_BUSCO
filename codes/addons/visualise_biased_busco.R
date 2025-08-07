8c9c95e4704e2c82eea333e631c69207ade93412
Jeremias Ivan <jeremias.ivan98@gmail.com>
2025-08-07 20:22:42 +1000

    Update visualisation

--- codes/addons/visualise_biased_busco.R
+++ codes/addons/visualise_biased_busco.R
@@ -1,37 +1,34 @@
 # This script aims to visualise proportion of biased BUSCO for each set of short reads
 # Written by: Jeremias Ivan
-# Last updated: 19 July 2025
+# Last updated: 07 August 2025
 
 library(tidyverse)
 
 df_metadata <- data.table:::fread("~/BusIER/data/eucs_shortreads.txt")
-dfpipe <- data.table::fread("prefix.cor_pipeline.sumtable")
-
-pipe_ln_busco <- length(unique(dfpipe$busco))
 
 dfpipe_bias <- dfpipe %>%
   group_by(read) %>%
+  mutate(total=n()) %>%
   filter(lm_p < 0.05 | spearman_p < 0.05) %>%
   mutate(note=ifelse(lm_p < 0.05 & spearman_p >= 0.05, "lm",
                      ifelse(lm_p < 0.05 & spearman_p < 0.05, "both", "sp"))) %>%
   group_by(read, note) %>%
-  summarise(n=n()/pipe_ln_busco*100) %>%
-  mutate(method="pipe")
+  summarise(n=n()/total*100) %>%
+  mutate(method="pipe") %>%
+  unique()
 
 dfpipe_bias_noout <- dfpipe %>%
   group_by(read) %>%
+  mutate(total=n()) %>%
   filter(lm_p_noout < 0.05 | spearman_p_noout < 0.05) %>%
   mutate(note=ifelse(lm_p_noout < 0.05 & spearman_p_noout >= 0.05, "lm",
                      ifelse(lm_p_noout < 0.05 & spearman_p_noout < 0.05, "both", "sp"))) %>%
   group_by(read, note) %>%
-  summarise(n=n()/pipe_ln_busco*100) %>%
-  mutate(method="pipe_noout")
+  summarise(n=n()/total*100) %>%
+  mutate(method="pipe_noout") %>%
+  unique()
 
 df_bias <- rbind(dfpipe_bias, dfpipe_bias_noout)
-df_bias$read <- sapply(df_bias$read, function(x) {
-  name <- df_metadata$species[df_metadata$id==x]
-  gsub("\\.", ". ", name)
-}) 
 
 ggplot(df_bias, aes(x=read, y=n)) +
   geom_bar(aes(group=read, fill=note, colour=note), stat="identity") +
@@ -43,7 +40,7 @@ ggplot(df_bias, aes(x=read, y=n)) +
     axis.title.x=element_blank(),
     axis.title.y=element_blank(),
     axis.text.y=element_text(size=20),
-    axis.text.x=element_text(size=15, angle=45, hjust=1, vjust=1),
+    axis.text.x=element_text(size=15, angle=45, hjust=1, vjust=1, face='italic'),
     legend.title=element_text(size=20),
     legend.text=element_text(size=20),
     legend.key.size=unit(1,"cm"),
